dc674015cb00ce59d1eee766e5cdfbd7ae1e0d56
bebaran <leandro.filho@sou.inteli.edu.br>
2025-08-14 15:38:36 -0300

    feat: automatizando a interação no terminal

---
+++ a.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ativar o ambiente virtual e rodar o chatbot
+
+# Ativa o venv
+source .venv/bin/activate
+
+# Executa o chatbot
+python3 chatboot.py
+# para rodar o chatbot, basta executar este script "./a.sh" no terminal
